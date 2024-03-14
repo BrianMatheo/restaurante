35e71177170ecc6d0af0a3ddf83dd419de24a191
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-03-14 14:52:53 -0500

    Creación del restaturante

---
+++ src/main/java/com/javalovers/restaurante/Restaurante.java
@@ -0,0 +1,17 @@
+package com.javalovers.restaurante;
+import java.util.Scanner;
+public class Restaurante {
+
+    public static void main(String[] args) {
+        int salir = 1;
+        Scanner scanner = new Scanner(System.in);
+        while(salir==1){
+            System.out.println("usted que tipo de persona es?");
+            String person = scanner.next();
+            switch(person){
+                case "estudiante":
+            }
+            
+        }
+    }
+}
